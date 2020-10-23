88fffb4b2492fbaa9910ffa76c0912221a8bed45
SeanUnland <69156579+SeanUnland@users.noreply.github.com>
2020-10-23 15:56:23 -0400

    completed sticky controller and tested on Postman

---
+++ models/person.js
@@ -0,0 +1,12 @@
+// const mongoose = require("../db/connection");
+
+// const Schema = mongoose.Schema;
+
+// const personSchema = new Schema({
+//   name: String,
+//   tasks: [{ ref: "Sticky", type: Schema.Type.ObjectId }],
+// });
+
+// const Person = mongoose.model("Person", personSchema);
+
+// module.exports = Person;
