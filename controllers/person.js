a75543410ed5ceffd9300a54e355d54f62aa3e76
SeanUnland <69156579+SeanUnland@users.noreply.github.com>
2020-10-23 15:15:55 -0400

    removed the imports from my person controller because Frieda made the person controller

--- controllers/person.js
+++ controllers/person.js
@@ -1,5 +1,3 @@
-const express = require("express");
-const router = require("express").Router();
-const mongoose = require("mongoose");
-const Person = require("../models/person");
-const db = mongoose.connection;
+
+
+
